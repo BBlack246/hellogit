2cf23c27ce0aa5021c0d3ece82e7f6d2a0eee81c
BBlack246 <BBlack246@users.noreply.github.com>
2019-02-07 15:01:26 -0400

    finished

---
+++ bb.cpp
@@ -0,0 +1,43 @@
+#include <vector>
+
+int main()
+{
+vector<char>vcomp;
+vector<int> v2={10,14,32,64,16};
+
+Vcomp.push_back("p");
+V2.pop_back();
+Vcomp.push_back("i");
+V2.pop_back();
+
+for (int i=0; i<4; i++)
+{
+V2.push_back(0);
+}
+for (int i-0;i< v2size(); i++)
+cout<<v2{i}<<"" "";
+
+for (auto v:v2)
+cout<<V<"";
+
+auto i=o;
+auto x = ou;
+auto ch ="A"
+auto v3 = v2;
+
+#include<string>
+string s0;
+string s1 ="hello";
+string s2 = s1 + " world";
+
+cout <<"s0="<<s0<<endl;
+cout<<"s1="<<s1<<endl;
+cout<<"The 4th character of s1 is< s1[3]<<endl;
+cout<<"s1 has"<<s1.size()<<"characters"<<endl;
+
+if(s1 == "hello")
+cout<<"hi there"<<endl;
+if (s2 ! +"world")
+cout<<"out of this world"<<endl;
+
+s1 +="Kitty"
