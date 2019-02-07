b5ad83cf788b5d82e6270210d1d528e4b2048df2
BBlack246 <BBlack246@users.noreply.github.com>
2019-02-07 15:15:40 -0400

    finished2

--- bb.cpp
+++ bb.cpp
@@ -3,22 +3,22 @@
 using namespace std;
 int main()
 {
-vector<char>Vcomp;
-vector<int> V2={10,14,32,64,16};
+vector<char>vcomp;
+vector<int> v2={10,14,32,64,16};
 
-Vcomp.push_back("p");
-V2.pop_back();
-Vcomp.push_back("i");
-V2.pop_back();
+vcomp.push_back("p");
+v2.pop_back();
+vcomp.push_back("i");
+v2.pop_back();
 
 for (int i=0; i<4; i++)
 {
-V2.push_back(0);
+v2.push_back(0);
 }
-for (int i-0;i< V2.size(); i++)
-cout<<V2{i}<<"" "";
+for (int i-0;i< v2.size(); i++)
+cout<<v2{i}<<"" "";
 
-for (auto V:V2)
-cout<<V<<"";
+for (auto v:v2)
+cout<<v<<"";
 
 }
