57658c342cefb15ed42ad5e2ce41d439112f2cb9
BBlack246 <BBlack246@users.noreply.github.com>
2019-02-07 15:20:04 -0400

    finished2

--- bb.cpp
+++ bb.cpp
@@ -6,17 +6,17 @@ int main()
 vector<char>vcomp;
 vector<int> v2={10,14,32,64,16};
 
-vcomp.push_back("p");
+vcomp.push_back('p');
 v2.pop_back();
-vcomp.push_back("i");
+vcomp.push_back('i');
 v2.pop_back();
 
 for (int i=0; i<4; i++)
 {
 v2.push_back(0);
 }
-for (int i-0;i< v2.size(); i++)
-cout<<v2{i}<<" ";
+for (int i=0;i<v2.size(); i++)
+cout<<v2[i]<<" ";
 
 for (auto v:v2)
 cout<<v<<"";
