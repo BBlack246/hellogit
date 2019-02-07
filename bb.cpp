44cb6a3a8f0615a7d5d43ce5e8a8089cb20b7c1d
BBlack246 <BBlack246@users.noreply.github.com>
2019-02-07 15:07:56 -0400

    finished2

--- bb.cpp
+++ bb.cpp
@@ -1,5 +1,6 @@
 #include <vector>
-
+#include <iostream>
+using namespace std;
 int main()
 {
 vector<char>vcomp;
@@ -20,25 +21,4 @@ cout<<v2{i}<<"" "";
 for (auto v:v2)
 cout<<V<"";
 
-auto i= "o";
-auto x = "ou";
-auto ch ="A"
-auto v3 = "v2";
-
-#include<string>
-string s0;
-string s1 ="hello";
-string s2 = s1 + " world";
-
-cout <<"s0="<<s0<<endl;
-cout<<"s1="<<s1<<endl;
-cout<<"The 4th character of s1 is<<s1[3]<<endl;
-cout<<"s1 has"<<s1.size()<<"characters"<<endl;
-
-if(s1 == "hello")
-cout<<"hi there"<<endl;
-if (s2 ! +"world")
-cout<<"out of this world"<<endl;
-
-s1 +="Kitty"
 }
